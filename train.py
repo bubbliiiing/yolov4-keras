f0bf5f6f32f46bdf2b337cfad38f3ca64e7e5882
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-06-01 08:29:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -205,7 +205,7 @@ if __name__ == "__main__":
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
     
-    freeze_layers = 367
+    freeze_layers = 249
     for i in range(freeze_layers): model_body.layers[i].trainable = False
     print('Freeze the first {} layers of total {} layers.'.format(freeze_layers, len(model_body.layers)))
 
