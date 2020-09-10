a50d36a42fbd6d76395239aee682a9db152f264f
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-10 14:19:48 +0800

    Update train.py

--- train.py
+++ train.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
     # 调整非主干模型first
     if True:
         Init_epoch = 0
-        Freeze_epoch = 25
+        Freeze_epoch = 50
         # batch_size大小，每次喂入多少数据
         batch_size = 8
         # 最大学习率
@@ -265,8 +265,8 @@ if __name__ == "__main__":
 
     # 解冻后训练
     if True:
-        Freeze_epoch = 25
-        Epoch = 50
+        Freeze_epoch = 50
+        Epoch = 100
         # batch_size大小，每次喂入多少数据
         batch_size = 2
 
