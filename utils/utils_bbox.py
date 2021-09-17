487861a5fa8ad80c75573c087c5c45c116b38882
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-09-17 17:09:48 +0800

    Update utils_bbox.py

--- utils/utils_bbox.py
+++ utils/utils_bbox.py
@@ -160,3 +160,111 @@ def DecodeBox(outputs,
     classes_out    = K.concatenate(classes_out, axis=0)
 
     return boxes_out, scores_out, classes_out
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    def sigmoid(x):
+        s = 1 / (1 + np.exp(-x))
+        return s
+    #---------------------------------------------------#
+    #   将预测值的每个特征层调成真实值
+    #---------------------------------------------------#
+    def get_anchors_and_decode(feats, anchors, num_classes):
+        # feats     [batch_size, 13, 13, 3 * (5 + num_classes)]
+        # anchors   [3, 2]
+        # num_classes 
+        # 3
+        num_anchors = len(anchors)
+        #------------------------------------------#
+        #   grid_shape指的是特征层的高和宽
+        #   grid_shape [13, 13] 
+        #------------------------------------------#
+        grid_shape = np.shape(feats)[1:3]
+        #--------------------------------------------------------------------#
+        #   获得各个特征点的坐标信息。生成的shape为(13, 13, num_anchors, 2)
+        #   grid_x [13, 13, 3, 1]
+        #   grid_y [13, 13, 3, 1]
+        #   grid   [13, 13, 3, 2]
+        #--------------------------------------------------------------------#
+        grid_x  = np.tile(np.reshape(np.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]), [grid_shape[0], 1, num_anchors, 1])
+        grid_y  = np.tile(np.reshape(np.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]), [1, grid_shape[1], num_anchors, 1])
+        grid    = np.concatenate([grid_x, grid_y], -1)
+        #---------------------------------------------------------------#
+        #   将先验框进行拓展，生成的shape为(13, 13, num_anchors, 2)
+        #   [1, 1, 3, 2]
+        #   [13, 13, 3, 2]
+        #---------------------------------------------------------------#
+        anchors_tensor = np.reshape(anchors, [1, 1, num_anchors, 2])
+        anchors_tensor = np.tile(anchors_tensor, [grid_shape[0], grid_shape[1], 1, 1]) 
+
+        #---------------------------------------------------#
+        #   将预测结果调整成(batch_size,13,13,3,85)
+        #   85可拆分成4 + 1 + 80
+        #   4代表的是中心宽高的调整参数
+        #   1代表的是框的置信度
+        #   80代表的是种类的置信度
+        #   [batch_size, 13, 13, 3 * (5 + num_classes)]
+        #   [batch_size, 13, 13, 3, 5 + num_classes]
+        #---------------------------------------------------#
+        feats           = np.reshape(feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
+        #------------------------------------------#
+        #   对先验框进行解码，并进行归一化
+        #------------------------------------------#
+        box_xy          = sigmoid(feats[..., :2]) + grid
+        box_wh          = np.exp(feats[..., 2:4]) * anchors_tensor
+        #------------------------------------------#
+        #   获得预测框的置信度
+        #------------------------------------------#
+        box_confidence  = sigmoid(feats[..., 4:5])
+        box_class_probs = sigmoid(feats[..., 5:])
+
+        box_wh = box_wh / 32
+        anchors_tensor = anchors_tensor / 32
+        fig = plt.figure()
+        ax = fig.add_subplot(121)
+        plt.ylim(-2,15)
+        plt.xlim(-2,15)
+        plt.scatter(grid_x,grid_y)
+        plt.scatter(5,5,c='black')
+        plt.gca().invert_yaxis()
+
+
+        anchor_left = grid_x - anchors_tensor/2 
+        anchor_top = grid_y - anchors_tensor/2 
+        print(np.shape(anchors_tensor))
+        print(np.shape(box_xy))
+        rect1 = plt.Rectangle([anchor_left[5,5,0,0],anchor_top[5,5,0,1]],anchors_tensor[0,0,0,0],anchors_tensor[0,0,0,1],color="r",fill=False)
+        rect2 = plt.Rectangle([anchor_left[5,5,1,0],anchor_top[5,5,1,1]],anchors_tensor[0,0,1,0],anchors_tensor[0,0,1,1],color="r",fill=False)
+        rect3 = plt.Rectangle([anchor_left[5,5,2,0],anchor_top[5,5,2,1]],anchors_tensor[0,0,2,0],anchors_tensor[0,0,2,1],color="r",fill=False)
+
+        ax.add_patch(rect1)
+        ax.add_patch(rect2)
+        ax.add_patch(rect3)
+
+        ax = fig.add_subplot(122)
+        plt.ylim(-2,15)
+        plt.xlim(-2,15)
+        plt.scatter(grid_x,grid_y)
+        plt.scatter(5,5,c='black')
+        plt.scatter(box_xy[0,5,5,:,0],box_xy[0,5,5,:,1],c='r')
+        plt.gca().invert_yaxis()
+
+        pre_left = box_xy[...,0] - box_wh[...,0]/2 
+        pre_top = box_xy[...,1] - box_wh[...,1]/2 
+
+        rect1 = plt.Rectangle([pre_left[0,5,5,0],pre_top[0,5,5,0]],box_wh[0,5,5,0,0],box_wh[0,5,5,0,1],color="r",fill=False)
+        rect2 = plt.Rectangle([pre_left[0,5,5,1],pre_top[0,5,5,1]],box_wh[0,5,5,1,0],box_wh[0,5,5,1,1],color="r",fill=False)
+        rect3 = plt.Rectangle([pre_left[0,5,5,2],pre_top[0,5,5,2]],box_wh[0,5,5,2,0],box_wh[0,5,5,2,1],color="r",fill=False)
+
+        ax.add_patch(rect1)
+        ax.add_patch(rect2)
+        ax.add_patch(rect3)
+
+        plt.show()
+        #
+    feat = np.random.normal(0,0.5,[4,13,13,75])
+    anchors = [[142, 110],[192, 243],[459, 401]]
+    get_anchors_and_decode(feat,anchors,20)
